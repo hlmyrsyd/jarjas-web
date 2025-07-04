f297b4dfc184f7ae877f4751bf9ef4bdc708f247
jodyboy <elite.crew66@gmail.com>
2025-07-04 17:01:41 +0700

    fix lenis error

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,33 @@
 
 import Lenis from "lenis";
 import { Hero, NavigationBar } from "./components";
+import { useEffect } from "react";
 
 export default function Home() {
-  const lenis = new Lenis({
-    autoRaf: true,
-  });
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
 
-  function raf(time:number) {
-    lenis.raf(time);
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
     requestAnimationFrame(raf);
-  }
 
-  requestAnimationFrame(raf);
+    return () => {
+      // Optional: clean up
+      lenis.destroy();
+    };
+  }, []);
 
   return (
     <div className="flex flex-col w-full h-[300vh] bg-[#1E1E1E]">
       <NavigationBar />
       <div className="flex flex-col px-60 py-10">
         <Hero />
-        
+
       </div>
     </div>
   );
